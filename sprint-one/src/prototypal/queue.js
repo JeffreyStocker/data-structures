47c2abf7fdbfd6740a49843b7fc2557772bf48e8
jeffreystocker <kagesennin@gmail.com>
2017-10-11 16:41:43 -0700

    Implement prototypal class implementation for both stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -2,7 +2,7 @@ var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
 
-  var someInstances = Object.create(Queue.prototype);
+  var someInstances = Object.create(queueMethods);
   someInstances.storage = {};
   someInstances.head = 0;
   someInstances.tail = 0;
@@ -10,31 +10,29 @@ var Queue = function() {
   return someInstances;
 };
 
-var queueMethods = {};
-
-Queue.prototype.enqueue = function(value) {
-  // add the value at the tail
-  this.storage[this.tail] = value;
-  // increase tail by 1
-  this.tail++;
-};
-
-Queue.prototype.dequeue = function(value) {
-  // if size > 0
-  if (this.size() > 0) {
-    // store head location in var headLocation
-    var headLocation = this.storage[this.head];
-    // store headLocation as var output
-    var output = headLocation;
-    // delete headLocation
-    delete headLocation;
-    // increase head++
-    this.head++;
-    // return output
-    return output;
+var queueMethods = {
+  enqueue: function(value) {
+    // add the value at the tail
+    this.storage[this.tail] = value;
+    // increase tail by 1
+    this.tail++;
+  }, 
+  dequeue: function(value) {
+    // if size > 0
+    if (this.size() > 0) {
+      // store head location in var headLocation
+      var headLocation = this.storage[this.head];
+      // store headLocation as var output
+      var output = headLocation;
+      // delete headLocation
+      delete headLocation;
+      // increase head++
+      this.head++;
+      // return output
+      return output;
+    }
+  },
+  size: function(value) {
+    return this.tail - this.head;
   }
-};
-
-Queue.prototype.size = function(value) {
-  return this.tail - this.head;
 };
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -2,7 +2,7 @@ var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   
-  var someInstances = Object.create(Stack.prototype);
+  var someInstances = Object.create(stackMethods);
   someInstances.storage = {};
   someInstances.storageSize = 0;
 
@@ -10,30 +10,28 @@ var Stack = function() {
   return someInstances;
 };
 
-var stackMethods = {};
-
-Stack.prototype.push = function(value) {
-  // take value and insert it onto storage at storageSize
-  this.storage[this.storageSize] = value;
-  // increase storage size
-  this.storageSize++;
-};
-
-Stack.prototype.pop = function() {
-  // check to see if size is > 0
-  if (this.size() > 0) {
-    // set storageLocation var to storage[storageSize - 1];
-    // set output var to storageLocation var
-    // delete storageLocation;
-    // return output
-    var storageLocation = this.storage[this.storageSize - 1];
-    var output = storageLocation;
-    delete storageLocation;
-    this.storageSize--;
-    return output;
+var stackMethods = {
+  push: function(value) {
+    // take value and insert it onto storage at storageSize
+    this.storage[this.storageSize] = value;
+    // increase storage size
+    this.storageSize++;
+  },
+  pop: function() {
+    // check to see if size is > 0
+    if (this.size() > 0) {
+      // set storageLocation var to storage[storageSize - 1];
+      // set output var to storageLocation var
+      // delete storageLocation;
+      // return output
+      var storageLocation = this.storage[this.storageSize - 1];
+      var output = storageLocation;
+      delete storageLocation;
+      this.storageSize--;
+      return output;
+    }
+  },
+  size: function() {
+    return this.storageSize;
   }
-};
-
-Stack.prototype.size = function() {
-  return this.storageSize;
 };
