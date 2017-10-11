f62f1fa8ceb59ff21c9958f234227500ed8d9c49
jeffreystocker <kagesennin@gmail.com>
2017-10-11 15:51:30 -0700

    "Passed- Stack Shared"

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,43 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+  someInstance.storage = {};
+  someInstance.head = 0;
+  someInstance.tail = 0;
+  
+  someInstance.enqueue = queueMethods.enqueue;
+  someInstance.dequeue = queueMethods.dequeue;
+  someInstance.size = queueMethods.size;
+  
+  return someInstance;
+
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function (value) {
+  // add value to tail location on storage
+  this.storage[this.tail] = value;
+  //increase tail value;
+  this.tail++;
+};
+
+
+queueMethods.dequeue = function () {
+  if (this.size() > 0) {
+    var headLocation = this.storage[this.head];
+    var output = headLocation;
+    delete headLocation;
+    this.head++;
+    return output;
+  }
+};
+
+queueMethods.size = function () {
+  return this.tail - this.head;
+};
+
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+  
+  someInstance.storage = {};
+  someInstance.storageSize = 0;
+  
+  someInstance.push = stackMethods.push;
+  someInstance.pop = stackMethods.pop;
+  someInstance.size = stackMethods.size;
+  
+  return someInstance;
 };
 
 var stackMethods = {};
 
 
+stackMethods.push = function (value) {
+  // input value set equal to the storage[storageSize]
+  this.storage[this.storageSize] = value;
+  // increase storageSize
+  this.storageSize++;
+  
+};
+
+stackMethods.pop = function () {
+  if (this.storageSize > 0) {
+    var storageLocation = this.storage[this.storageSize - 1];
+    var output = storageLocation;
+    delete storageLocation;
+    this.storageSize--;
+    return output;
+  }
+};
+
+stackMethods.size = function () {
+  return this.storageSize;
+};
