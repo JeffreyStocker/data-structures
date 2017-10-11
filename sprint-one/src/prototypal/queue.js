ba101653bce92c2f3458a7d3ee742de050178fb4
jeffreystocker <kagesennin@gmail.com>
2017-10-11 16:26:19 -0700

    prototypal mostly done, getting 2 errors

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  var someInstances = Object.create(Queue.prototype);
+  someInstances.storage = {};
+  someInstances.head = 0;
+  someInstances.tail = 0;
+  
+  return someInstances;
 };
 
 var queueMethods = {};
 
+Queue.prototype.enqueue = function(value) {
+  // add the value at the tail
+  this.storage[this.tail] = value;
+  // increase tail by 1
+  this.tail++;
+};
 
+Queue.prototype.dequeue = function(value) {
+  // if size > 0
+  if (this.size() > 0) {
+    // store head location in var headLocation
+    var headLocation = this.storage[this.head];
+    // store headLocation as var output
+    var output = headLocation;
+    // delete headLocation
+    delete headLocation;
+    // increase head++
+    this.head++;
+    // return output
+    return output;
+  }
+};
+
+Queue.prototype.size = function(value) {
+  return this.tail - this.head;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  var someInstances = Object.create(Stack.prototype);
+  someInstances.storage = {};
+  someInstances.storageSize = 0;
+
+
+  return someInstances;
 };
 
 var stackMethods = {};
 
+Stack.prototype.push = function(value) {
+  // take value and insert it onto storage at storageSize
+  this.storage[this.storageSize] = value;
+  // increase storage size
+  this.storageSize++;
+};
 
+Stack.prototype.pop = function() {
+  // check to see if size is > 0
+  if (this.size() > 0) {
+    // set storageLocation var to storage[storageSize - 1];
+    // set output var to storageLocation var
+    // delete storageLocation;
+    // return output
+    var storageLocation = this.storage[this.storageSize - 1];
+    var output = storageLocation;
+    delete storageLocation;
+    this.storageSize--;
+    return output;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.storageSize;
+};
