99a774f854337586e7a0b28c01ada610bd2ab01e
jeffreystocker <kagesennin@gmail.com>
2017-10-12 10:46:23 -0700

    Implements linked lists and passes all those test yall

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -1,15 +1,59 @@
 var LinkedList = function() {
-  var list = {};
+  var list = {}; //{value: "somevalue", pointer: {next object} } 
   list.head = null;
   list.tail = null;
 
   list.addToTail = function(value) {
+    // set obj = node(value);
+    var obj = Node(value);
+    
+    // if empty, head and tail are null
+    if (list.head === null && list.tail === null) {
+      // point list.head to obj
+      list.head = obj;
+      // point list.tail to obj
+      list.tail = obj;
+    } else {
+      // point current list.tail node.next to obj
+      list.tail.next = obj;
+      // point list.tail to obj
+      list.tail = obj;
+    }
   };
 
   list.removeHead = function() {
+    if (list.head === null) {
+      console.log('what are you doing man');
+    } else {
+      // store current head as output
+      var output = list.head;
+      // move list.head to output.next
+      list.head = output.next;
+      // delete output.next
+      delete output.next;
+      // return output.value
+      return output.value;
+    }
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    // set node to head if node is undefined
+    node = node || list.head;
+    
+    // if value is target.value
+      // return true
+    // else if list.tail = node
+      // return false
+    // else
+      // recurse to next node
+      
+    if (target === node.value) {
+      return true;
+    } else if (list.tail === node) {
+      return false;
+    } else {
+      return list.contains(target, node.next);
+    }
   };
 
   return list;
