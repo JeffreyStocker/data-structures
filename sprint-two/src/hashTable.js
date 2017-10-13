860af08ea23a45fc315965aae54bc53bacb6d29e
jeffreystocker <kagesennin@gmail.com>
2017-10-12 22:28:18 -0700

    "adjusted to add both the index, and key into the hash table, but need to add conditioners"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -10,13 +10,48 @@ HashTable.prototype.insert = function(k, v) {
   
   /// k is index number, while v is value
   // uses limitset to store v in this._storage;
-  this._storage.set(index, v);
+  // debugger;
+  // if (this._storage.get(index + x) !== undefined) {
+  //   this.insert(k, v, x + 1);
+  // } else {
+  //   this._storage.set(index + x, [v, index + x]);
+  // }
+  var toPushToTree = {index: k, value: v};
+  
+  if (this._storage.get(index) !== undefined) {
+    var toChange = this._storage.get(index);
+    for (var i = 0; i < toChange.length; i++) {
+      if (toChange[i] === k) {
+        /// need to add some checks to see if there a duplicate index
+        /// if collision 
+          //then overwrite
+        // else no collision 
+          //then add another array. 
+      }
+    }
+    toChange.push(toPushToTree);
+    this._storage.set(index, toChange);
+  } else {
+    this._storage.set(index, [toPushToTree]);
+  }
+  
   
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  return output = this._storage.get (index);
+  // var output = this._storage.get (index);
+  var node = this._storage.get (index);
+
+  if (!node.length || node.length > 1) {
+    for (let i of node) {
+      if (i === k) {
+        console.log ('i:' + i);
+        return i;
+      }
+    }
+  }
+  return node[0].value;
   // uses limitedarray.get to retieve the value of index
 };
 
