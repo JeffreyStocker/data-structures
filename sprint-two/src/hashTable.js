301361aa068b6311225576705a32a8addbe43a54
jeffreystocker <kagesennin@gmail.com>
2017-10-12 21:01:45 -0700

    "Hashtable: Passing all but does not handle collisions"

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,14 +7,27 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  
+  /// k is index number, while v is value
+  // uses limitset to store v in this._storage;
+  this._storage.set(index, v);
+  
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  return output = this._storage.get (index);
+  // uses limitedarray.get to retieve the value of index
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // this._storage.each ( (element, i, collection)=>{
+  //   if (element === k) {
+      
+  //   }
+  // });
+  this._storage.set (index, undefined);
 };
 
 
