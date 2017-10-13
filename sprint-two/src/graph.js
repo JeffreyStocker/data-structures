339e7f4697f4da2cb22063929b54652ef6d2eed3
James Terry <jamesnorthterry@gmail.com>
2017-10-12 19:51:45 -0700

    Implements graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,12 +2,12 @@
 
 // Instantiate a new graph
 var Graph = function() {
-  
+
   // graph object
   var graph = Object.create(Graph.prototype);
   this.nodeHolder = {};
   this.size = 0;
-  
+
   // connection object
   //index number ++
 };
@@ -30,7 +30,7 @@ Graph.prototype.contains = function(value) {
     // if one of them contains the value
       // return true
   // otherwise it isn't there and we return false
-  
+
   for (var key in this.nodeHolder) {
     if (this.nodeHolder[key].value === value) {
       return true;
@@ -44,12 +44,13 @@ Graph.prototype.removeNode = function(value) {
   // var storage to hold node
   var storage = this.nodeHolder[value];
   for (var linkedNode in storage.edges) {
-    delete linkedNode.edge[value];
+    // delete linkedNode.edges[value];
+    // console.log(linkedNode);
+    this.removeEdge(value, linkedNode)
   }
   delete this.nodeHolder[value];
   // remove edge from linked nodes to this particular Node
   // remove node from nodeHolder
-  
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
@@ -78,13 +79,15 @@ Graph.prototype.addEdge = function(fromNode, toNode) {
 Graph.prototype.removeEdge = function(fromNode, toNode) {
   // fromNode = this.nodeHolder[fromNode];
   // toNode = this.nodeHolder[toNode];
-  
-  delete this.nodeHolder[fromNode].edge[toNode];
-  delete this.nodeHolder[toNode].edge[fromNode];
+  delete this.nodeHolder[fromNode].edges[toNode];
+  delete this.nodeHolder[toNode].edges[fromNode];
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  for (var node in this.nodeHolder) {
+    cb(this.nodeHolder[node].value);
+  }
 };
 
 /*
@@ -93,10 +96,10 @@ Graph.prototype.forEachNode = function(cb) {
 
 var Nodes = function (value) {
   var node = {};
-  
+
   node.value = value; //value
   node.id = value; //ID
   node.edges = {}; //edges
-  
+
   return node;
 };
